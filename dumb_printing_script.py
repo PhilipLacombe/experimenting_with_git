6c0bcfb4d75a807bdbb357871ad30a0606560ade
Lacombe, Philip <philip.lacombe@barco.com>
2022-01-22 13:21:21 +0100

    Add some printing to the script

---
+++ dumb_printing_script.py
@@ -0,0 +1,15 @@
+def print_some_numbers():
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
+    print(6)
+    print(7)
+    print(8)
+    print(9)
+    print(10)
+
+
+if __name__ == "__main__":
+    print_some_numbers()
